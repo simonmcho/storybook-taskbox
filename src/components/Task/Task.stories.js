97f95b949d05f762cce01e2bac432a4a8724637e
Simon Cho <simonmcho@gmail.com>
2020-01-03 10:49:06 -0800

    Tasklist added to expand on CDD

--- src/components/Task/Task.stories.js
+++ src/components/Task/Task.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
 
-import Task from './Task'
+import Task from '.'
 
 export default {
   component: Task,
---
+++ src/components/TaskList/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import pt from 'prop-types'
+
+import Task from '../Task'
+
+const TaskList = ({
+  loading,
+  tasks,
+  onPinTask,
+  onArchiveTask
+}) => {
+  const events = {
+    onPinTask,
+    onArchiveTask
+  }
+
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="list-items">
+      {
+        tasks.map((task) => (
+          <Task key={task.id} task={task} {...events} />
+        ))
+      }
+    </div>
+  )
+}
+
+TaskList.propTypes = {
+  loading: pt.bool,
+  tasks: pt.arrayOf(Task.propTypes.task).isRequired,
+  onPinTask: pt.func.isRequired,
+  onArchiveTasK: pt.func.isRequired
+}
+
+TaskList.defaultProps = {
+  loading: false
+}
+
+export default TaskList
